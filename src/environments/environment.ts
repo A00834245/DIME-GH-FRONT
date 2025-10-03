13085e95f0a16fd60f3434af52227b1b951c3210
Maruca Cantu Valdes <A00834245@tec.mx>
2025-10-03 13:29:29 -0600

    Arquitectura MF

--- src/environments/environment.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-  production: false,
-  googleMapsApiKey: process.env['NG_APP_GOOGLE_MAPS_API_KEY'] || '',
-  googleMapId: process.env['NG_APP_GOOGLE_MAP_ID'] || '',
-  googleDatasetId: process.env['NG_APP_GOOGLE_DATASET_ID'] || '',
-  googleStyleId: process.env['NG_APP_GOOGLE_STYLE_ID'] || '',
-  // TODO: Add NG_APP_GOOGLE_PROJECT_ID to .env file
-  googleProjectId: process.env['NG_APP_GOOGLE_PROJECT_ID'] || 'your-project-id'
-};
