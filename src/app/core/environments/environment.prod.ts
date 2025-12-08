a244f571171d608fa391cfdd88482cc3b8624a9e
SantiagoTorresHernandez <a00830563@itesm.mx>
2025-12-08 09:25:42 -0600

    login bug, still not fixed

--- src/app/core/environments/environment.prod.ts
+++ src/app/core/environments/environment.prod.ts
@@ -5,16 +5,16 @@
 export const environment = {
   production: true,
   msal: {
-    clientId: process.env['NG_APP_AZURE_CLIENT_ID'] || '',
-    authority: `https://${process.env['NG_APP_AZURE_AUTHORITY_DOMAIN']}/${process.env['NG_APP_AZURE_TENANT_NAME']}/${process.env['NG_APP_AZURE_LOGIN_USER_FLOW']}`,
-    redirectUri: process.env['NG_APP_AZURE_REDIRECT_URI'] || '',
-    postLogoutRedirectUri: process.env['NG_APP_AZURE_REDIRECT_URI'] || '',
-    knownAuthorities: [process.env['NG_APP_AZURE_AUTHORITY_DOMAIN'] || '']
+    clientId: import.meta.env['NG_APP_AZURE_CLIENT_ID'] || '',
+    authority: `https://${import.meta.env['NG_APP_AZURE_AUTHORITY_DOMAIN']}/${import.meta.env['NG_APP_AZURE_TENANT_NAME']}/${import.meta.env['NG_APP_AZURE_LOGIN_USER_FLOW']}`,
+    redirectUri: import.meta.env['NG_APP_AZURE_REDIRECT_URI'] || '',
+    postLogoutRedirectUri: import.meta.env['NG_APP_AZURE_REDIRECT_URI'] || '',
+    knownAuthorities: [import.meta.env['NG_APP_AZURE_AUTHORITY_DOMAIN'] || '']
   },
   // Google Maps
-  googleMapsApiKey: process.env['NG_APP_GOOGLE_MAPS_API_KEY'] || '',
-  googleMapId: process.env['NG_APP_GOOGLE_MAP_ID'] || '',
-  googleDatasetId: process.env['NG_APP_GOOGLE_DATASET_ID'] || '',
-  googleStyleId: process.env['NG_APP_GOOGLE_STYLE_ID'] || '',
-  googleProjectId: process.env['NG_APP_GOOGLE_PROJECT_ID'] || ''
+  googleMapsApiKey: import.meta.env['NG_APP_GOOGLE_MAPS_API_KEY'] || '',
+  googleMapId: import.meta.env['NG_APP_GOOGLE_MAP_ID'] || '',
+  googleDatasetId: import.meta.env['NG_APP_GOOGLE_DATASET_ID'] || '',
+  googleStyleId: import.meta.env['NG_APP_GOOGLE_STYLE_ID'] || '',
+  googleProjectId: import.meta.env['NG_APP_GOOGLE_PROJECT_ID'] || ''
 };
---
+++ src/app/core/environments/environment.template.ts
@@ -0,0 +1,9 @@
+export const environment = {
+    production: false,
+    msal: {
+      clientId: 'CHANGEME',
+      authority: 'https://login.microsoftonline.com/TENANT_ID',
+      redirectUri: 'http://localhost:4200'
+    },
+  };
+  
